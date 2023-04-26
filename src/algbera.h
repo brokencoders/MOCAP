db0e675de0ceda3d71912d4adc00efb6d9a27024
bose-coder <79697089+bose-coder@users.noreply.github.com>
2023-04-26 19:21:51 +0200

    QR cholesky decomposition, fix transpose in place

--- src/algbera.h
+++ src/algbera.h
@@ -10,6 +10,7 @@
 #include <cstdlib>
 #include <immintrin.h>
 #include <tuple>
+#include <cfloat>
 
 namespace Algebra
 {
@@ -33,7 +34,8 @@ namespace Algebra
         Matrix(Matrix &&mat);
         ~Matrix();
 
-        Matrix &operator=(const Matrix &m);
+        Matrix &operator=(const Matrix &m) &;
+        Matrix &operator=(const Matrix &m) && = delete;
 
         Matrix  operator+(const Matrix &m) const &;
         Matrix& operator+(const Matrix &m) && { return operator+=(m); }
@@ -51,8 +53,7 @@ namespace Algebra
         Matrix& operator-=(const Matrix &m);
         Matrix& operator-=(double s);
 
-        Matrix  operator*(const Matrix &m) const &;
-        Matrix& operator*(const Matrix &m) && { return operator*=(m); }
+        Matrix  operator*(const Matrix &m) const;
         Matrix  operator*(double s) const &;
         Matrix& operator*(double s) && { return operator*=(s); }
         Matrix& operator*=(const Matrix &m);
@@ -78,11 +79,12 @@ namespace Algebra
         Matrix  hom_i() const &;
         Matrix& hom_i() &&;
 
-        Matrix solve(const Matrix& b) const;
+        Matrix solve(Matrix b) const;
+        Matrix cholesky() const;
         std::tuple<Matrix, Matrix> QR() const;
         std::tuple<Matrix, Matrix, Matrix> svd() const;
 
-        void reshape(size_t row, size_t col);
+        Matrix& reshape(size_t row, size_t col);
 
         double &operator[](size_t i);
         double operator[](size_t i) const;
@@ -100,17 +102,18 @@ namespace Algebra
         Matrix  getRow(int64_t r) const &;
         Matrix& getRow(int64_t r) &&;
         Matrix  subMatrix(int64_t top, int64_t left, int64_t bottom = -1, int64_t right = -1) const &;
-        Matrix& subMatrix(int64_t top, int64_t left, int64_t bottom = -1, int64_t right = -1) &&;
 
         void print() const;
         Matrix& transpose();
-        Matrix T() const;
+        Matrix T() const &;
+        Matrix T() &&;
 
     private:
         static void realTranspose(const double* A, double* B, const size_t r, const size_t c, const size_t lda, const size_t ldb);
-        static void realTransposeInPlace(double* A, const size_t r, const size_t c, const size_t lda);
+        static void realTransposeInPlace(double*& A, const size_t r, const size_t c, const size_t lda, bool local_buff);
         static Matrix householderReflect(const Matrix& u);
-        void householderReflectSubM(const Matrix& u, size_t r, size_t c);
+        void householderReflectSubMat(const Matrix& u, size_t r, size_t c);
+        void householderReflectSubMatForward(const Matrix& u, size_t r, size_t c, bool store = true);
 
     private:
         double *m;
@@ -125,14 +128,40 @@ namespace Algebra
         size_t getSize() const { return size; }
 
         friend void zero(Matrix&);
-        friend Matrix identity(size_t);
-        friend Matrix operator+(double, const Matrix&);
-        friend Matrix operator-(double, const Matrix&);
-        friend Matrix operator*(double, const Matrix&);
-        friend Matrix vstack(const std::vector<Matrix>& mats);
-        friend Matrix hstack(const std::vector<Matrix>& mats);
+        friend Matrix  identity(size_t);
+        friend Matrix  operator+(double, const Matrix&);
+        friend Matrix& operator+(double, Matrix&&);
+        friend Matrix  operator-(double, const Matrix&);
+        friend Matrix& operator-(double, Matrix&&);
+        friend Matrix  operator*(double, const Matrix&);
+        friend Matrix& operator*(double, Matrix&&);
+        friend double  operator/(double, const Matrix&);
+        friend Matrix  vstack(const std::vector<Matrix>& mats);
+        friend Matrix  hstack(const std::vector<Matrix>& mats);
     };
 
+    void zero(Matrix& mat);
+    Matrix identity(size_t size);
+
+/* 
+    class MatrixView
+    {
+    private:
+        MatrixView(double* data, size_t row, size_t col, size_t row_offset)
+            : m(data), row(row), col(col), roff(row_offset) {}
+
+    public:
+        MatrixView(const MatrixView&) = delete;
+        MatrixView(const MatrixView&&) = delete;
+        MatrixView& operator=(const MatrixView&) = delete;
+
+    private:
+        double *m;
+        size_t row, col, roff;
+        
+        friend class Matrix;
+    };
+ */
     using Vector = Matrix;
 
 #ifdef ALGEBRA_IMPL
@@ -195,7 +224,10 @@ namespace Algebra
             m = buff;
         else
             m = (double*)std::aligned_alloc(32, size * sizeof(double));
-        realTranspose(mat.m, m, mat.row, mat.col, mat.row, row);
+        if (row > 1 && col > 1)
+            realTranspose(mat.m, m, mat.row, mat.col, mat.row, row);
+        else
+            std::copy(mat.m, mat.m + mat.size, m);
     }
 
     Matrix::Matrix(Matrix &&mat)
@@ -219,10 +251,10 @@ namespace Algebra
     {
         if (m != buff && m)
             std::free(m);
-        std::cout << "Matrix destructor\n";
+        // std::cout << "Matrix destructor\n";
     }
 
-    Matrix &Matrix::operator=(const Matrix &mat)
+    Matrix &Matrix::operator=(const Matrix &mat) &
     {
         if (m != buff)
             std::free(m);
@@ -262,6 +294,11 @@ namespace Algebra
         for (size_t i = 0; i < mat.size; i++)
             sum.m[i] = mat.m[i] + s;
         return sum;
+    }
+
+    Matrix& operator+(double s, Matrix&& mat)
+    {
+        return mat.operator+=(s);
     }
 
     Matrix& Matrix::operator+=(const Matrix &m)
@@ -315,6 +352,11 @@ namespace Algebra
         return sub;
     }
 
+    Matrix& operator-(double s, Matrix&& mat)
+    {
+        return mat.operator-=(s);
+    }
+
     Matrix Matrix::operator-(double s) const &
     {
         Matrix sub(row, col);
@@ -341,7 +383,7 @@ namespace Algebra
 
     /* --------- MULT --------- */
 
-    Matrix Matrix::operator*(const Matrix &mat) const &
+    Matrix Matrix::operator*(const Matrix &mat) const
     {
         if (col != mat.row)
             throw std::length_error("Matrix sizes not matching in multiplication operation");
@@ -381,6 +423,11 @@ namespace Algebra
         return mul;
     }
 
+    Matrix& operator*(double s, Matrix&& mat)
+    {
+        return mat.operator*=(s);
+    }
+
     Matrix& Matrix::operator*=(const Matrix &mat)
     {
         if (col != mat.row)
@@ -394,12 +441,12 @@ namespace Algebra
             if (this->m == buff)
                 realTranspose(m, tmp_buff, row, col, row, col);
             else
-                realTransposeInPlace(m, row, col, col);
+                realTransposeInPlace(m, row, col, col, m == buff);
         }
         else
         {
             new_m = (double*)std::aligned_alloc(32, row * mat.col * sizeof(double));
-            realTransposeInPlace(m, row, col, col);
+            realTransposeInPlace(m, row, col, col, m == buff);
         }
 
         for (size_t i = 0; i < row; i++)
@@ -474,6 +521,13 @@ namespace Algebra
         for (size_t i = 0; i < size; i++)
             m[i] /= s;
         return *this;
+    }
+
+    double operator/(double s, const Matrix& mat)
+    {
+        if (mat.col != 1 || mat.row != 1)
+            throw std::length_error("Matrix is not 1x1 in scalar matrix division operation");
+        return s / mat.m[0];
     }
 
     Matrix::operator double() const
@@ -570,30 +624,96 @@ namespace Algebra
         return *this;
     }
 
-    Matrix Matrix::solve(const Matrix &b) const
+    Matrix Matrix::solve(Matrix b) const
     {
-        return Matrix(0);
+        Matrix x(col, b.col), R(*this);
+        auto min = std::min(row-1, col);
+        zero(x);
+
+        for (size_t i = 0; i < min; i++)
+        {
+            Vector v = R.subMatrix(i,i, -1,i);
+            v[0] += v[0] < 0 ? -v.norm() : v.norm();
+            if (v.m[0] > DBL_EPSILON || v.m[0] < -DBL_EPSILON)
+            {
+                v /= v[0];
+                R.householderReflectSubMatForward(v, i,i, false);
+                b.householderReflectSubMat(v, i,0);
+            }
+        }
+        for (int64_t i = x.row - 1; i >= 0; i--)
+        {
+            auto divisor = R.m[i*col + i];
+            if (divisor > DBL_EPSILON || divisor < -DBL_EPSILON)
+                x.setRow(i, (b.getRow(i) - R.getRow(i)*x)/divisor);
+        }
+
+        return x;
+    }
+
+    Matrix Matrix::cholesky() const
+    {
+        if (row != col)
+            throw std::length_error("Matrix not square in Matrix::cholesky()");
+        
+        Matrix L(row, col);
+        for (size_t i = 0; i < row; i++)
+        {
+            size_t j;
+            for (j = 0; j < i; j++)
+                L.m[i*col + j] = 0;
+            for (; j < col; j++)
+                L.m[i*col + j] = m[i*col + j];
+        }
+
+        for (size_t i = 0; i < L.col; i++)
+        {
+            double& a11 = L.m[i*col + i];
+            a11 = sqrt(a11);
+            if (std::isnan(a11))
+                throw std::invalid_argument("Matrix not positive defined in Matrix::cholesky()");
+
+            double a11_i = 1./a11;
+            for (size_t j = i+1; j < row; j++)
+                L.m[j + i*col] *= a11_i;
+
+            for (size_t j = i+1; j < row; j++)
+                for (size_t k = j; k < col; k++)
+                    L.m[j*col + k] -= L.m[j + i*col] * L.m[k + i*col];
+        }
+
+        return L.T();
     }
 
     std::tuple<Matrix, Matrix> Matrix::QR() const
     {
-        Matrix Q(row, row), R(*this);
-        auto min = std::min(row, col);
+        std::tuple<Matrix, Matrix> tpl = {identity(row), *this};
+        auto& [Q, R] = tpl;
+        auto min = std::min(row-1, col);
 
         for (size_t i = 0; i < min; i++)
         {
-            Matrix v = R.subMatrix(i,i, -1,i);
-            double norm = v.normSquare();
-            v[0] += v[0] < 0 ? -norm : norm;
-            v /= v[0];
-            R.householderReflectSubM(v, i,i);
-            
-            double tau_i = 2 / v.normSquare();
+            Vector v = R.subMatrix(i,i, -1,i);
+            v[0] += v[0] < 0 ? -v.norm() : v.norm();
+            if (v.m[0] > DBL_EPSILON || v.m[0] < -DBL_EPSILON)
+            {
+                v /= v[0];
+                R.householderReflectSubMatForward(v, i,i);
+            }
         }
-        
+        for (int64_t i = min - 1; i >= 0; i--)
+        {
+            Vector u = R.subMatrix(i,i, -1,i);
+            if (u.m[0] > DBL_EPSILON || u.m[0] < -DBL_EPSILON)
+            {
+                u.m[0] = 1.;
+                Q.householderReflectSubMat(u, i,i);
+            }
+        }
 
-
-        return {Q,R};
+        for (size_t i = 1; i < row; i++)
+            std::fill(R.m + i*col, R.m + i*col + i, 0.);
+        return tpl;
     }
 
     std::tuple<Matrix, Matrix, Matrix> Matrix::svd() const
@@ -601,11 +721,12 @@ namespace Algebra
         return {Matrix(0), Matrix(0), Matrix(0)};
     }
 
-    void Matrix::reshape(size_t row, size_t col)
+    Matrix& Matrix::reshape(size_t row, size_t col)
     {
         if (size != row * col)
-            throw std::length_error("Matrix columns not matching for vertical stacking");
+            throw std::length_error("Matrix wrong reshape size");
         this->row = row, this->col = col;
+        return *this;
     }
 
     double &Matrix::operator[](size_t i)
@@ -778,7 +899,7 @@ namespace Algebra
         size_t endr = std::max(row - r, mat.row);
         size_t endc = std::max(col - c, mat.col);
         for (size_t i = 0; i < endr; i++)
-            std::copy(mat.m + i*mat.col, mat.m + i*mat.col + endc, m + (r+i)*col + r);
+            std::copy(mat.m + i*mat.col, mat.m + i*mat.col + endc, m + (r+i)*col + c);
     }
 
     Matrix Matrix::getCol(int64_t c) const &
@@ -868,53 +989,18 @@ namespace Algebra
 
     Matrix Matrix::subMatrix(int64_t t, int64_t l, int64_t b, int64_t r) const &
     {
-        if (t < 0) t += col;
+        if (t < 0) t += row;
         if (l < 0) l += col;
-        if (b < 0) b += col;
+        if (b < 0) b += row;
         if (r < 0) r += col;
         if (l >= col || l < 0 || r >= col || r < 0 || t >= row || t < 0 || b >= row || b < 0)
             throw std::out_of_range("Matrix coordinates out of range in Matrix::subMatrix");
-        Matrix sm(col);
+        if (t > b || l > r)
+            throw std::out_of_range("Matrix coordinates negatively overlapping in Matrix::subMatrix &&");
+        Matrix sm(b-t + 1, r-l + 1);
         for (size_t i = t; i <= b; i++)
-            std::copy(m + i*col + l, m + i*col + r, sm.m);
+            std::copy(m + i*col + l, m + i*col + r + 1, sm.m + (i-t)*sm.col);
         return sm;
-    }
-
-    Matrix& Matrix::subMatrix(int64_t t, int64_t l, int64_t b, int64_t r) &&
-    {
-        if (t < 0) t += col;
-        if (l < 0) l += col;
-        if (b < 0) b += col;
-        if (r < 0) r += col;
-        if (l >= col || l < 0 || r >= col || r < 0 || t >= row || t < 0 || b >= row || b < 0)
-            throw std::out_of_range("Matrix coordinates out of range in Matrix::subMatrix &&");
-        size_t ncol = r-l, nrow = b-t;
-        if (nrow*ncol <= buff_size)
-        {
-            if (m == buff)
-                for (size_t i = 0; i < nrow; i++)
-                    std::copy(m + (t+i)*col + l, m + (t+i)*col + r + 1, buff + i*ncol);
-            else
-            {
-                for (size_t i = 0; i < nrow; i++)
-                    std::copy(m + (t+i)*col + l, m + (t+i)*col + r + 1, buff + i*ncol);
-                std::free(m);
-                m = buff;
-            }
-        }
-        else
-        {
-            double* new_buf = (double*)std::aligned_alloc(32, nrow*ncol);
-            for (size_t i = 0; i < row; i++)
-                std::copy(m + (t+i)*col + l, m + (t+i)*col + r + 1, new_buf + i*ncol);
-            std::free(m);
-            m = new_buf;
-        }
-
-        col = ncol;
-        row = nrow;
-        size = ncol*nrow;
-        return *this;
     }
 
     void Matrix::print() const
@@ -935,30 +1021,37 @@ namespace Algebra
 
     Matrix& Matrix::transpose()
     {
-        realTransposeInPlace(m, row, col, col);
+        if (row > 1 && col > 1)
+            realTransposeInPlace(m, row, col, col, m == buff);
         std::swap(row, col);
         return *this;
     }
 
-    Matrix Matrix::T() const
+    Matrix Matrix::T() const &
     {
-        return {*this, true};
+        return Matrix(*this, true);
+    }
+
+    Matrix Matrix::T() &&
+    {
+        return transpose();
     }
 
     void Matrix::realTranspose(const double *A, double *B, const size_t r, const size_t c, const size_t lda, const size_t ldb)
     {
-        const size_t max_r = r & ~(1L);
-        const size_t max_c = c & ~(1L);
+        const size_t block_size = 4UL;
+        const size_t max_r = r & ~(block_size-1);
+        const size_t max_c = c & ~(block_size-1);
 
-        #pragma omp parallel for
-        for (size_t i = 0; i < max_r; i += 4)
+        //#pragma omp parallel for
+        for (size_t i = 0; i < max_r; i += block_size)
         {
-            for (size_t j = 0; j < max_c; j += 4)
-                for (size_t k = 0; k < 4; k++)
-                    for (size_t l = 0; l < 4; l++)
-                        B[(j + l)*ldb + (i + k)] = A[(i + k)*lda + j + l];
+            for (size_t j = 0; j < max_c; j += block_size)
+                for (size_t k = 0; k < block_size; k++)
+                    for (size_t l = 0; l < block_size; l++)
+                        B[(j + l)*ldb + (i + k)] = A[(i + k)*lda + (j + l)];
 
-            for (size_t k = 0; k < 4; k++)
+            for (size_t k = 0; k < block_size; k++)
                 for (size_t j = max_c; j < c; j++)
                     B[j*ldb + (i + k)] = A[(i + k)*lda + j];
         }
@@ -968,41 +1061,79 @@ namespace Algebra
                 B[j*ldb + i] = A[i*lda + j];
     }
 
-    void Matrix::realTransposeInPlace(double *A, const size_t r, const size_t c, const size_t lda)
+    void Matrix::realTransposeInPlace(double*& A, const size_t r, const size_t c, const size_t lda, bool lb)
     {
-        const size_t max_r = r & ~(1L);
-        const size_t max_c = c & ~(1L);
-
-        #pragma omp parallel for
-        for (size_t i = 0; i < max_r; i += 4)
+        if (lb)
         {
-            for (size_t j = 0; j < max_c; j += 4)
-                for (size_t k = 0; k < 4; k++)
-                    for (size_t l = 0; l < 4; l++)
-                        std::swap(A[(j + l)*r + (i + k)], A[(i + k)*c + j + l]);
-
-            for (size_t k = 0; k < 4; k++)
-                for (size_t j = max_c; j < c; j++)
-                    std::swap(A[j*r + (i + k)], A[(i + k)*c + j]);
+            alignas(32) double tmp_buff[buff_size];
+            std::copy(A, A + r*c, tmp_buff);
+            realTranspose(tmp_buff, A, r, c, lda, r);
         }
-
-        for (size_t i = max_r; i < r; i++)
-            for (size_t j = 0; j < c; j++)
-                std::swap(A[j*r + i], A[i*c + j]);
+        else
+        {
+            double* tmp_buff = (double*)std::aligned_alloc(32, r*c * sizeof(double));
+            realTranspose(A, tmp_buff, r, c, lda, r);
+            std::free(A);
+            A = tmp_buff;
+        }
     }
 
     Matrix Matrix::householderReflect(const Matrix &u)
     {
         double tau_i = 2 / u.normSquare();
-        Matrix ref = (-u/tau_i)*u.T();
+        Matrix ref = -tau_i*u*u.T();
         for (size_t i = 0; i < u.getSize(); i++)
             ref.val(i,i) += 1.;
         return ref;
     }
 
-    void Matrix::householderReflectSubM(const Matrix &u, size_t r, size_t c)
+    void Matrix::householderReflectSubMat(const Matrix &u, size_t r, size_t c)
     {
+        double tau_i = 2 / u.normSquare();
+        Vector wt(1, col - c);
 
+        for (size_t i = 0; i < wt.size; i++)
+        {
+            wt.m[i] = m[r*col + i + c];
+            for (size_t k = 1; k < u.size; k++)
+                wt.m[i] += u.m[k] * m[(r+k)*col + i + c];
+            wt.m[i] *= tau_i;
+        }
+        for (size_t i = 0; i < wt.size; i++)
+            m[r*col + c + i] -= wt.m[i];
+        for (size_t i = 1; i < u.size; i++)
+            for (size_t j = 0; j < wt.size; j++)
+                m[(r+i)*col + c + j] -= u.m[i] * wt.m[j];
+    }
+
+    void Matrix::householderReflectSubMatForward(const Matrix &u, size_t r, size_t c, bool store)
+    {
+        double tau_i = 2 / u.normSquare(), theta = m[r*col + c];
+        Vector wt(1, col - c - 1);
+
+        for (size_t k = 1; k < u.size; k++)
+            theta += u.m[k] * m[(r+k)*col + c];
+        m[r*col + c] -= tau_i * theta;
+
+        if (store)
+            for (size_t i = 1; i < u.size; i++)
+                m[(r+i)*col + c] = u.m[i];
+        else
+            for (size_t i = 1; i < u.size; i++)
+                m[(r+i)*col + c] = 0;
+        
+        for (size_t i = 0; i < wt.size; i++)
+        {
+            wt.m[i] = m[r*col + i + c+1];
+            for (size_t k = 1; k < u.size; k++)
+                wt.m[i] += u.m[k] * m[(r+k)*col + i + c+1];
+            wt.m[i] *= tau_i;
+        }
+        for (size_t i = 0; i < wt.size; i++)
+            m[r*col + c+1 + i] -= wt.m[i];
+        for (size_t i = 1; i < u.size; i++)
+            for (size_t j = 0; j < wt.size; j++)
+                m[(r+i)*col + c+1 + j] -= u.m[i] * wt.m[j];
     }
 
     void zero(Matrix& mat)
